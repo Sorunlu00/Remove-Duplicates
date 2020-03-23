8eb1f6063ce6d72a9eb7e1bb3ea36181fb6c28b0
kxf3 <61079248+kxf3@users.noreply.github.com>
2020-03-23 13:58:58 +0000

    Upgraded. exe file also included.

--- Remove.py
+++ Remove.py
@@ -6,9 +6,13 @@ with open("duplicates.txt", 'r+')as email:
     for x in lines:
         if x not in na_duplicate:
             na_duplicate.append(x)
-    print("\tYou have had lines:", len(lines))
-    print('\tDuplicates removed from the list is: ', len(lines) - len(na_duplicate), "\n")
     print(*na_duplicate, sep=" \n")
     print('\n')
+    print("\tYou have had lines:", len(lines))
+    print('\tDuplicates removed from the list is: ', len(lines) - len(na_duplicate), "\n")
+    print('Has been copied to your clipboard!')
+    print('\n')
     pd.DataFrame(na_duplicate).to_clipboard(excel=True, header=False, index=False)
-    print(input('Press any key to exit.'))
+    print('\t\t\t\t\tAuthor: kxf3/Sorunlu00')
+    print(input('Press Enter key to exit.'))
+
